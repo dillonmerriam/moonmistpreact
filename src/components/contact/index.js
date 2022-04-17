6294e79b6cfaa189750f262d1d15d9cd224bcce8
Dillon Merriam <merriamdillon@yahoo.com>
2022-04-17 00:50:24 -0700

    contact form done, sections with links to section done

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,15 +1,27 @@
 import style from './style.css'; 
 
 const Contact = () => (
-	<div class={style.contact} id='contact'>
-    <div id='contact-form'>
-      information form
+  <div class={style.contactcontainer} id='contact'>
+  <span>Contact</span>
+  <div class={style.contactform}>
+    <div class={style.usermessage}>
+      <textarea type="text" id="message" name="message" cols="50" rows="10"/>
     </div>
-    <div id='user-message'>
-      user message
+    <div class={style.userinfo}>
+      <form>
+        <label for="fname">First name:</label><br/>
+        <input type="text" id="fname" name="fname"/><br/>
+        <label for="lname">Last name:</label><br/>
+        <input type="text" id="lname" name="lname"/><br/>
+        <label for="email">Email:</label><br/>
+        <input type="text" id="email" name="email"/><br/>
+      </form>
+      <button>Submit</button>
     </div>
 
   </div>
+  
+</div>
 )
 
 export default Contact;
