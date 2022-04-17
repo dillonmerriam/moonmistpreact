383e7247c7729b325c194ba3b645080a1c173848
Dillon Merriam <merriamdillon@yahoo.com>
2022-04-16 17:48:23 -0700

    sections created

--- src/components/app.js
+++ src/components/app.js
@@ -2,17 +2,22 @@ import { h } from 'preact';
 import { Router } from 'preact-router';
 
 import Header from './header';
+import About from './about';
+import Contact from './contact';
+import Hero from './hero';
 
 // Code-splitting is automated for `routes` directory
 import Home from '../routes/home';
+import Testimonials from './testimonials';
 
 
 const App = () => (
 	<div id="app">
 		<Header />
-
-			
-
+		<Hero />
+		<About />
+		<Testimonials />
+		<Contact />
 	</div>
 )
 
